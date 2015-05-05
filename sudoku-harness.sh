5e03fc47dec4eb77761a3a9b8bd4c496873e1039
Jason Price <jason.price@weather.com>
2015-05-05 10:41:16 -0700

    sudoku-harness now takes a directory name as a required arg. also works

--- sudoku-harness.sh
+++ sudoku-harness.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+if [ -d $1 ]; then
+	DIR=$1
+else
+	echo "USAGE: $0 <directory of sudoku solvers>"
+fi
+
+rootdir=$(dirname $0)
+
+if [ "$rootdir" == "." ]; then
+	rootdir=$(pwd)
+fi
+
+cd $DIR
+
 for f in ./*.py ; do
-	cat ../puzzles.txt | while read pname puzzle solution; do
+	echo -n "starting solver $f"
+	cat ${rootdir}/puzzles.txt | while read pname puzzle solution; do
+		echo -n " $pname"
 		result=$(timeout 60 gtime --output=${f}@${pname}@time1 python ${f} ${puzzle} 2>&1)
 		if [ "$result" != "$solution" ]; then
 			echo "file $f failed to solve puzzle $puzzle"
@@ -11,9 +27,11 @@ for f in ./*.py ; do
 		fi
 		result=$(timeout 60 gtime --output=${f}@${pname}@time2 python ${f} ${puzzle} 2>&1)
 		result=$(timeout 60 gtime --output=${f}@${pname}@time3 python ${f} ${puzzle} 2>&1)
-		for res in ${f}.${pname}.time?; do
+		for res in ${f}@${pname}@time?; do
 			t=$(cat $res | grep elapsed)
+		done
 	done
+	echo " done"
 done
 
-../compare.py
+${rootdir}/compare.py
